ef3a1356ad4f2e60b6d51f89c22a0eaaf65e72a9
Minurte1 <hohoangphucjob@gmail.com>
2024-10-16 20:08:47 +0700

    update

--- DFrontEnd Không Có Chat AI/src/dashbroand/src/_mock/account.js
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: "Tuấn Anh",
-  email: "quocanhanh1204@gmail.com",
-  photoURL: "../../assets/images/avatars/avata1.jpg",
-};
